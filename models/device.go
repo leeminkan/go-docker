1fcc5d3c783f717e553eb97a629b386a43cbff38
leeminkan <16520950@gm.uit.edu.vn>
2020-07-25 12:42:15 +0700

    clean phat code, update migation

--- models/device.go
+++ models/device.go
@@ -84,3 +84,16 @@ func (d *Device) Update(device_name string, os string, machine_id string) error 
 
 	return nil
 }
+
+func ExistDevice(machineID string) (bool, error) {
+	var device Device
+	err := db.Select("id").Where("deleted_on = ? AND machine_id = ? ", 0, machineID).First(&device).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return false, err
+	}
+	if device.ID > 0 {
+		return true, nil
+	}
+
+	return false, nil
+}
