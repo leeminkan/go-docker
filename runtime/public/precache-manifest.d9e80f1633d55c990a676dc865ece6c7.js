8495e69dea1d961604e74ea7512b5ff49c210405
Nguyen An Thoi <nguyen.an.thoi.212@gmail.com>
2020-07-30 16:47:19 +0700

    build

---
+++ runtime/public/precache-manifest.d9e80f1633d55c990a676dc865ece6c7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a197c8a3eb87feea204679c5da87d9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbbe677bbdd5b9e6e7f1",
+    "url": "/static/css/2.41f07c40.chunk.css"
+  },
+  {
+    "revision": "52601d1c65f975e45e69",
+    "url": "/static/css/main.bdeecb0e.chunk.css"
+  },
+  {
+    "revision": "bbbe677bbdd5b9e6e7f1",
+    "url": "/static/js/2.c1a4e4f3.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.c1a4e4f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52601d1c65f975e45e69",
+    "url": "/static/js/main.ca0ad321.chunk.js"
+  },
+  {
+    "revision": "5f6dcee0bffca74a6503",
+    "url": "/static/js/runtime-main.bac5efaf.js"
+  },
+  {
+    "revision": "9d149f79ad800f6841c7ea86b2094a99",
+    "url": "/static/media/register.9d149f79.jpg"
+  }
+]);
