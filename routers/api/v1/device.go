ee98ef07a6bb439bd5d88a3d8b6e87ba6c9b99ac
phantrungphat <phatphan3598@gmail.com>
2020-07-30 13:59:23 +0700

    fix

--- routers/api/v1/device.go
+++ routers/api/v1/device.go
@@ -924,7 +924,7 @@ func DeleteImage(c *gin.Context) {
 	}
 
 	// //control device stop container throw mqtt
-	// container, err := device_service.GetContainer(form.ContainerID)
+	// container, err := device_service.Get
 
 	// if err != nil {
 	// 	appG.Response(http.StatusBadRequest, e.ERROR_DEVICE_GET_CONTAINER_FAIL, nil)
